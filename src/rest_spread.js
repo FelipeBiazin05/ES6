a6be667645e1504f887ae5aca363db6a2e91d4c5
Felipe <felipegabrielsilvabiazin@gmail.com>
2024-10-24 13:17:39 -0300

    Feito dia 24-10-2024

--- src/rest_spread.js
+++ src/rest_spread.js
@@ -1,3 +1,5 @@
+// REST
+
 function somar() {
     let soma = 0;
 
@@ -11,3 +13,54 @@ function somar() {
 }
 
 console.log(somar(10, 20, 30))
+
+function somarComRest(...numeros) {
+    const soma = numeros.reduce((total, numeroAtual) => {
+        total += numeroAtual;
+        return total;
+    }, 0)
+    return soma;
+}
+
+console.log(somarComRest(10, 20, 30))
+
+// SPREAD
+
+const numeros = [1, 2, 3, 4]
+console.log(...numeros)
+
+const timesDeFutebolDeSp = ['santos', 'palmeiras', 'corinthians', 'são paulo'];
+const timesDeFutebolDoRio = ['vasco', 'botafogo', 'flamengo', 'fluminense'];
+
+// const timesDeFutebol = timesDeFutebolDeSp.concat(timesDeFutebolDoRio);
+const timesDeFutebol = [...timesDeFutebolDeSp, ...timesDeFutebolDoRio]
+
+console.log(timesDeFutebol);
+
+const carroDaJulia = {
+    modelo: 'gol',
+    marca: 'vw',
+    motor: 1.6
+}
+
+const carroDaAna = {
+    ...carroDaJulia,
+    motor: 1.8
+}
+
+console.log(carroDaAna);
+
+// desestruturação
+// const motorDoCarroDaAna = carroDaAna.motor;
+const {motor: motorDoCarroDaAna} = carroDaAna;
+const {motor: motorDoCarroDaJulia} = carroDaJulia;
+
+console.log(motorDoCarroDaAna);
+console.log(motorDoCarroDaJulia);
+
+const [item1, item2, item3, ...outrosTimes] = timesDeFutebol;
+
+console.log(item1);
+console.log(item2);
+console.log(item3);
+console.log(outrosTimes);
